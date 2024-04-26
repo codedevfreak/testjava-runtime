d90f12e1ed8bf0a5e0246b532b4ef21c44ddc27c
codedevfreak <kr15.last933@gmail.com>
2024-04-26 18:19:12 +0700

    First Commit

---
+++ javaRuntime1/src/javaRuntime1/Runtime1.java
@@ -0,0 +1,12 @@
+package javaRuntime1;
+
+import java.io.IOException;
+
+public class Runtime1 {
+
+	public static void main(String[] args) throws IOException {
+		Runtime.getRuntime().exec("notepad");//will open a new notepad 
+
+	}
+
+}
---
+++ javaRuntime1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module javaRuntime1 {
+}
