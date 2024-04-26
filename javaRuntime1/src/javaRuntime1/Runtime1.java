38a7f98a10bf46e3fb3ceaab9f60ed7aad5e1241
codedevfreak <kr15.last933@gmail.com>
2024-04-26 18:24:52 +0700

    Edited Runtime.java

--- javaRuntime1/src/javaRuntime1/Runtime1.java
+++ javaRuntime1/src/javaRuntime1/Runtime1.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Runtime1 {
 
 	public static void main(String[] args) throws IOException {
-		Runtime.getRuntime().exec("notepad");//will open a new notepad 
+		Runtime.getRuntime().exec("mspaint");//will open a new notepad 
 
 	}
 
